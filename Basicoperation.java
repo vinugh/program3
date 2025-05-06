6772c7f2c546d382bf5d9cb01b7e8adac0e90f02
vinugh <01fe23bca166@kletech.ac.in>
2025-05-06 16:03:54 +0530

    save file

---
+++ Basicoperation.java
@@ -0,0 +1,16 @@
+package Student;
+
+public class Basicoperation {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int quotient = a / b;
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+    }
+}
